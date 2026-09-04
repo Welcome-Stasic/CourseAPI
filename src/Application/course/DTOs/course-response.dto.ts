9689a4777c2a56fd1dcb18a3c05c3ce9b5f7ef38
stanislav <stanislav.glumov.2006@mail.ru>
2026-09-04 15:05:32 +0500

    метадата для dto

--- src/Application/course/DTOs/course-response.dto.ts
+++ src/Application/course/DTOs/course-response.dto.ts
@@ -1,8 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CourseResponseDto {
+  @ApiProperty()
   id: string;
+  @ApiProperty()
   title: string;
+  @ApiProperty()
   description: string;
+  @ApiProperty()
   duration: number;
+  @ApiProperty({ type: String, format: 'date-time' })
   createdAt: Date;
+  @ApiProperty({ type: String, format: 'date-time' })
   updatedAt: Date;
 }
--- src/Application/course/DTOs/create-course.dto.ts
+++ src/Application/course/DTOs/create-course.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCourseDto {
+  @ApiProperty({ example: 'NestJS Course' })
   title: string;
+
+  @ApiProperty({ example: 'Backend development course' })
   description: string;
+
+  @ApiProperty({ example: 24, type: Number })
   duration: number;
 }
